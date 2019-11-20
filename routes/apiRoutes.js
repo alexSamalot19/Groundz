def8bcf9bbb1b12bd7c6107234d2060bc3029c81
Owais Master <owais.master@uconn.edu>
2019-11-19 20:56:46 -0500

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -70,6 +70,39 @@ module.exports = function (app) {
     }
   });
 
+  app.get"/api/examples/:parkName", async (req, res) => {
+
+    axios.get("http://dataservice.accuweather.com/locations/v1/poi/search?apikey=DpQ5M5BPR9XrRItZA3yJImF8J1taR52k&q=06901").then(
+      function (response) {
+        console.log("The response object: " + response);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+
+    try {
+      console.log("It worked!")
+    } catch (error) {
+      res.status(400).json({ error: { name: error.name, msg: error.message } });
+    }
+  });
 
 
 };
