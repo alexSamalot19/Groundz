d17facbfcf2bd915e507683628d312c584bb4973
Owais Master <owais.master@uconn.edu>
2019-11-21 20:58:15 -0500

    api weather link

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -40,9 +40,14 @@ module.exports = function (app) {
     // const { data } =
     const parkData = await axios.get(
       "https://developer.nps.gov/api/v1/parks?stateCode=" +
-        stateName +
-        "&api_key=eYWf8cdqhJTjLKiKn6EpzpRvttfMm8ARxeyJFk6Z"
+      stateName +
+      "&api_key=eYWf8cdqhJTjLKiKn6EpzpRvttfMm8ARxeyJFk6Z"
     );
+    
+    // const weather = await axios.get(
+    //   "api.openweathermap.org/data/2.5/weather?lat=90&lon=-50&APPID=b426febecc2d6e6409afe07084149a30"
+    // );
+    // console.log(weather);
 
     const parkNames = parkData.data.data.map(it => ({
       text: "A",
@@ -58,7 +63,30 @@ module.exports = function (app) {
     } catch (error) {
       res.status(400).json({ error: { name: error.name, msg: error.message } });
     }
-  });;
+
+  });
 };
+  // app.get("/api/parks/weather", async (req, res) => {
+  //   const { stateName } = req.params;
+  //   // const { data } =
+  //   const parkData = await axios.get(
+  //     "api.openweathermap.org/data/2.5/weather?lat=40&lon=50&APPID=b426febecc2d6e6409afe07084149a30"
+  //   );
+  //   //console.log(parkData);
+//   //   const parkNames = parkData.data.data.map(it => ({
+//       text: "A",
+//       description: stateName,
+//       park: it.name
+//     }));
 
+//     //console.log(parkNames);
 
+//     try {
+//       await db.Example.bulkCreate(parkNames);
+//       res.json(parkNames);
+//     } catch (error) {
+//       res.status(400).json({ error: { name: error.name, msg: error.message } });
+//     }
+
+//   });
+// };
